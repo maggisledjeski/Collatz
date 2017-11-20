ed4171e41a7fd56e894ca725bd450a8c9a6d5663
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-11-20 14:46:02 -0500

    initial python code

---
+++ python/collatz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+uinput = int(raw_input("Enter a number: "))
+print "you entered", uinput
+if uinput%2 == 0:
+    print uinput, "is even."
+else:
+    print uinput, "is odd."
+    
+
