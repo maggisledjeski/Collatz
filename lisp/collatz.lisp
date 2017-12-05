f6b20c26dbc9c721f40f917894e942b76db9ad1b
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-12-05 10:56:06 -0500

    fixed count to use conditionals

--- lisp/collatz.lisp
+++ lisp/collatz.lisp
@@ -9,27 +9,32 @@
 (defvar num)
 (setf num 0)
 
-(loop 
-    (if (= (mod N 2) 0)
-        (progn
-        ;(format t "~D is even." N)
-        (setf N (/ N 2))
-        (format t "~D~%" N)
-        (setf num (+ num 1)))
-    )
-
-    (if (= N 1)
-        (return N)
-    )
-    
-    (if (= (mod N 2) 1)
-        (progn
-        ;(format t "~D is odd." N)
-        (setf N (+ (* N 3) 1))
-        (format t "~D~%" N)
-        (setf num (+ num 1)))
-    )
-    
-    (when (= N 1) (return N))
+(loop while (> N 1) do
+    (cond ((= (mod N 2) 0)
+    (setf N (/ N 2)))
+    (t (setf N (+ 1 (* N 3)))))
+    (setf num (+ num 1))
 )
+;    (if (= (mod N 2) 0)
+;        (progn
+;        ;(format t "~D is even." N)
+;        (setf N (/ N 2))
+;        (format t "~D~%" N)
+;        (setf num (+ num 1)))
+;    )
+;
+;    (if (= N 1)
+;        (return N)
+;    )
+;    
+;    (if (= (mod N 2) 1)
+;        (progn
+;        ;(format t "~D is odd." N)
+;        (setf N (+ (* N 3) 1))
+;        (format t "~D~%" N)
+;        (setf num (+ num 1)))
+;    )
+;    
+;    (when (= N 1) (return N))
+;)
 (format t "Count of numbers in the sequence: ~D~%" num)
