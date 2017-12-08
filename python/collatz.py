b0173a73d8b570aab0fab508811d1d8193762af6
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-12-08 12:46:23 -0500

    sorted python

--- python/collatz.py
+++ python/collatz.py
@@ -54,7 +54,36 @@ while control != 0:
     if uinput < numbers[dupIndex].getNum() and dup == 1:
         numbers[dupIndex].setNum(uinput)
     control = control - 1
+print "sorted by step count"
+n = len(numbers)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if numbers[j].getSteps() < numbers[j+1].getSteps():
+            temp = numbers[j].getSteps()
+            x = numbers[j+1].getSteps()
+            numbers[j].setSteps(x)
+            numbers[j+1].setSteps(temp)
+            temp2 = numbers[j].getNum()
+            y = numbers[j+1].getNum()
+            numbers[j].setNum(y)
+            numbers[j+1].setNum(temp2)
 
 for i in range(0,10):
     print numbers[i].getNum(),"    ",numbers[i].getSteps()
 
+print "sorted by number"
+n = len(numbers)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if numbers[j].getNum() < numbers[j+1].getNum():
+            temp = numbers[j].getNum()
+            x = numbers[j+1].getNum()
+            numbers[j].setNum(x)
+            numbers[j+1].setNum(temp)
+            temp2 = numbers[j].getSteps()
+            y = numbers[j+1].getSteps()
+            numbers[j].setSteps(y)
+            numbers[j+1].setSteps(temp2)
+
+for i in range(0,10):
+    print numbers[i].getNum(),"    ",numbers[i].getSteps()
