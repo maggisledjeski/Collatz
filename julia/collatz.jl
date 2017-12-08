c009c4ce4516ed3f5953edfdba7735b069c68ba1
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-12-08 10:37:20 -0500

    sorted julia

--- julia/collatz.jl
+++ julia/collatz.jl
@@ -36,9 +36,38 @@ while j > 1
     end
     j -= 1
 end
-
+println("sorted by step count")
+ for i in 2:length(sArray)
+    for j in 1:length(sArray)-1
+      if sArray[j] < sArray[j+1]
+          tmp = sArray[j]
+          sArray[j] = sArray[j+1]
+          sArray[j+1] = tmp
+          tep2 = nArray[j]
+          nArray[j] = nArray[j+1]
+          nArray[j+1] = tep2  
+      end
+    end
+  end
 for x in 1:10
     println(nArray[x],"     ",sArray[x])
 end
+println("sorted by number")
+for i in 2:length(nArray)
+    for j in 1:length(nArray)-1
+      if nArray[j] < nArray[j+1]
+          tmp = nArray[j]
+          nArray[j] = nArray[j+1]
+          nArray[j+1] = tmp
+          tmp2 = sArray[j]
+          sArray[j] = sArray[j+1]
+          sArray[j+1] = tmp2
+      end
+    end
+  end
+for x in 1:10
+    println(nArray[x],"     ",sArray[x])
+end
+
 exit(0)
 
