71193307834999cee1d69bb8c5e16855bb404989
Margaret Sledjeski <sledjeski_ma@cobra.cs.mercer.edu>
2017-12-11 21:27:46 -0500

    commented

--- python/collatz.py
+++ python/collatz.py
@@ -1,64 +1,77 @@
 #!/usr/bin/python
 
-class Number:
-    def __init__(self, num, steps):
+class Number:   #a class that holds both a number that will go into the collatz sequence and the step count to finish that sequence
+    def __init__(self, num, steps): #initializer method
         self.num = num
         self.steps = steps
 
-    def setSteps(self, steps):
+    def setSteps(self, steps):  #setter steps method
         self.steps = steps
     
-    def setNum(self, num):
+    def setNum(self, num):  #setter number method
         self.num = num
 
-    def getSteps(self):    
+    def getSteps(self):    #getter step method
         return self.steps  
 
-    def getNum(self):    
+    def getNum(self):   #getter number method
         return self.num
-  
+ 
+uinput = long(raw_input("Enter a number: "))    #user input is stored in uinput
+numbers = []    #array that holds the top 10 highest step counts with their sequence numbers
 
-uinput = int(raw_input("Enter a number: "))
-print "you entered", uinput
-numbers = []
-
+#adds that Numbers to the array and sets the num and steps to 0
 for i in range(0,10):
     numbers.append(Number(0,0))
 
-control = uinput
+control = uinput    #variable that controls when the all of the sequence numbers have been tested
 
+#uses the control variable to stop the while loop until every sequence number starting from uinput is tested
 while control != 0:
-    uinput = control
-    count = 0
+    uinput = control    #sets uinput to control (current sequence number), to make sure the current sequence number is being tested
+    count = 0   #sets the step count to 0
+    
+    #computes the sequence count until uinput = 1
     while uinput != 1:
-        if uinput%2 == 0:
+        if uinput%2 == 0:   #tests if the uinput is even
             uinput = uinput/2
-        else:
+        else:   #uinput is odd
             uinput = (uinput*3)+1
-        count = count + 1    
-    uinput = control
-    dup = 0
-    dupIndex = 0
-    smallIndex = 0
-    small = numbers[0].getSteps()
+        count = count + 1   #increments step count
+    uinput = control    #resets uinput to be equal to the current sequence number
+    dup = 0     #duplicate flag
+    dupIndex = 0    #index of a number with a duplicate step count
+    smallIndex = 0  #index with the smallest number of steps
+    small = numbers[0].getSteps()   #number with the smallest step count
+    
+    #goes through the array numbers to see which element has the smallest step count and if there are duplicate sequence numbers with the same step count
     for i in range(0,10):
-        if numbers[i].getSteps() <= small:
-            small = numbers[i].getSteps()
-            smallIndex = i
-        if numbers[i].getSteps() == count:
-            dup = 1
-            dupIndex = i
+        if numbers[i].getSteps() <= small:  #checks to see which element has the smallest step count
+            small = numbers[i].getSteps()   #reassigns the number with the smallest number of steps
+            smallIndex = i  #saves the index with the smallest number of steps
+        if numbers[i].getSteps() == count:  #checks to see if there is a number in the array with the same step count as the current sequence number
+            dup = 1 #set duplicate flag to true
+            dupIndex = i    #saves the index where the duplicate step count occurs
+    
+    #if the current sequence number being tested has a higher step count than the smallest step count in the array and the step count is not 
+    #a duplicate replace the information at the smallest index to match the current sequence number
     if count > numbers[smallIndex].getSteps() and dup == 0:
         numbers[smallIndex].setNum(uinput)
         numbers[smallIndex].setSteps(count)
+    
+    #if the current sequence number being tested is < the sequence number at the duplicate index and the duplicate flag is true, set the number 
+    #at the duplicate index to the current sequence number
     if uinput < numbers[dupIndex].getNum() and dup == 1:
         numbers[dupIndex].setNum(uinput)
-    control = control - 1
-print "sorted by step count"
-n = len(numbers)
+    control = control - 1   #decrement sequence number
+
+#sorted by step count
+print "sorted by step count size:"
+n = len(numbers)    #length of the array
 for i in range(n):
     for j in range(0, n-i-1):
         if numbers[j].getSteps() < numbers[j+1].getSteps():
+            #swaps the 2 Numbers steps and num by using 2 temporary variables to store the information
             temp = numbers[j].getSteps()
             x = numbers[j+1].getSteps()
             numbers[j].setSteps(x)
@@ -67,15 +80,17 @@ for i in range(n):
             y = numbers[j+1].getNum()
             numbers[j].setNum(y)
             numbers[j+1].setNum(temp2)
-
+#prints sorted array
 for i in range(0,10):
     print numbers[i].getNum(),"    ",numbers[i].getSteps()
 
-print "sorted by number"
-n = len(numbers)
+#sorted by squence number
+print "sorted by sequence number:"
+n = len(numbers)    #length of the array
 for i in range(n):
     for j in range(0, n-i-1):
         if numbers[j].getNum() < numbers[j+1].getNum():
+            #swaps the 2 Numbers steps and num by using 2 temporary variables to store the information
             temp = numbers[j].getNum()
             x = numbers[j+1].getNum()
             numbers[j].setNum(x)
@@ -84,6 +99,6 @@ for i in range(n):
             y = numbers[j+1].getSteps()
             numbers[j].setSteps(y)
             numbers[j+1].setSteps(temp2)
-
+#prints the sorted array
 for i in range(0,10):
     print numbers[i].getNum(),"    ",numbers[i].getSteps()
--- python/recursed/Rcollatz.py
+++ python/recursed/Rcollatz.py
@@ -1,67 +1,79 @@
 #!/usr/bin/python
 
-class Number:
-    def __init__(self, num, steps):
+class Number:   #a class that holds both a number that will go into the collatz sequence and the step count to finish that sequence
+    def __init__(self, num, steps): #initializer method
         self.num = num
         self.steps = steps
 
-    def setSteps(self, steps):
+    def setSteps(self, steps):  #setter method of steps
         self.steps = steps
     
-    def setNum(self, num):
+    def setNum(self, num):  #setter method of num
         self.num = num
 
-    def getSteps(self):    
+    def getSteps(self):    #getter method for steps
         return self.steps  
 
-    def getNum(self):    
+    def getNum(self):    #getter method for num
         return self.num
 
-def seq_length(n):
-    count = 0 
-    if n == 1:
+def seq_length(n):  #recursive method to get the number of steps of a sequence for a number n
+    count = 0   #set step count to 0
+    if n == 1:  #if n = 1 return the step count
         return count
-    if n%2 == 0:
-        count = (seq_length(int(n/2)) +1)
-    elif n%2 != 0:
-        count = (seq_length(int(3*n+1)) + 1)
+    if n%2 == 0:    #tests if n is even
+        count = (seq_length(long(n/2)) +1)  #stores the recursive call of n/2 then adds 1 to count
+    elif n%2 != 0:  #n is odd
+        count = (seq_length(long(3*n+1)) + 1)   #stores the recursive call of 3*n+1 then adds 1 to count
     return count
 
-uinput = int(raw_input("Enter a number: "))
-print "you entered", uinput
-numbers = []
+#gets the user input and stores it in uinput
+uinput = long(raw_input("Enter a number: "))
+numbers = []    #array that holds the 10 sequence numbers with the highest step counts
 
+#adds a Number to every element in the array and sets the steps and the num to 0
 for i in range(0,10):
     numbers.append(Number(0,0))
 
-control = uinput
+control = uinput    #variable that controls when the all of the sequence numbers have been tested
 
+#uses the control variable to stop the while loop until every sequence number starting from uinput is tested
 while control != 0:
-    uinput = control
-    count = seq_length(uinput)  #recursive call to determine sequence length
-    dup = 0
-    dupIndex = 0
-    smallIndex = 0
-    small = numbers[0].getSteps()
+    uinput = control    #sets uinput to control (current sequence number), to make sure the current sequence number is being tested
+    count = seq_length(uinput)  #recursive call to determine the step length for uinput
+    dup = 0     #duplicate flag
+    dupIndex = 0    #index of a number with a duplicate step count
+    smallIndex = 0  #index with the smallest number of steps
+    small = numbers[0].getSteps()   #number with the smallest step count
+    
+    #goes through the array numbers to see which element has the smallest step count and if there are duplicate sequence numbers with the same step count
     for i in range(0,10):
-        if numbers[i].getSteps() <= small:
-            small = numbers[i].getSteps()
-            smallIndex = i
-        if numbers[i].getSteps() == count:
-            dup = 1
-            dupIndex = i
+        if numbers[i].getSteps() <= small:  #checks to see which element has the smallest step count
+            small = numbers[i].getSteps()   #reassigns the number with the smallest number of steps
+            smallIndex = i  #saves the index with the smallest number of steps
+        if numbers[i].getSteps() == count:  #checks to see if there is a number in the array with the same step count as the current sequence number
+            dup = 1 #duplicate flag set to true
+            dupIndex = i    #saves the index where the duplicate step count occurs
+    
+    #if the current sequence number being tested has a higher step count than the smallest step count in the array and the step count is not 
+    #a duplicate replace the information at the smallest index to match the current sequence number
     if count > numbers[smallIndex].getSteps() and dup == 0:
         numbers[smallIndex].setNum(uinput)
         numbers[smallIndex].setSteps(count)
+    
+    #if the current sequence number being tested is < the sequence number at the duplicate index and the duplicate flag is true, set the number 
+    #at the duplicate index to the current sequence number
     if uinput < numbers[dupIndex].getNum() and dup == 1:
         numbers[dupIndex].setNum(uinput)
-    control = control - 1
+    control = control - 1   #decrements the sequence number
 
-print "sorted by step count"
-n = len(numbers)
+#sorted by step count
+print "sorted by step count size:"
+n = len(numbers)    #length of the array
 for i in range(n):
     for j in range(0, n-i-1):
         if numbers[j].getSteps() < numbers[j+1].getSteps():
+            #swaps the 2 elements steps and num by using 2 temporary variables to store the information
             temp = numbers[j].getSteps()
             x = numbers[j+1].getSteps()
             numbers[j].setSteps(x)
@@ -70,15 +82,17 @@ for i in range(n):
             y = numbers[j+1].getNum()
             numbers[j].setNum(y)
             numbers[j+1].setNum(temp2)
-
+#prints sorted array
 for i in range(0,10):
     print numbers[i].getNum(),"    ",numbers[i].getSteps()
 
-print "sorted by number"
-n = len(numbers)
+#sorted by sequence number
+print "sorted by sequence number:"
+n = len(numbers)    #length of the array
 for i in range(n):
     for j in range(0, n-i-1):
         if numbers[j].getNum() < numbers[j+1].getNum():
+            #swaps the 2 elements steps and num by using 2 temporary variables to store the information
             temp = numbers[j].getNum()
             x = numbers[j+1].getNum()
             numbers[j].setNum(x)
@@ -87,7 +101,7 @@ for i in range(n):
             y = numbers[j+1].getSteps()
             numbers[j].setSteps(y)
             numbers[j+1].setSteps(temp2)
-
+#prints the sorted array
 for i in range(0,10):
     print numbers[i].getNum(),"    ",numbers[i].getSteps()
 
